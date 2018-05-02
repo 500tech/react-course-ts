6a9c7ac5f6dfdd4c579185b7d5cf2fcb8f58fb73
Shay Keinan <>
2018-05-02 11:48:08 +0300

    passing props

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const Header = () => (
-  React.createElement('h1', null, 'react and course')
+const Header = (props) => (
+  React.createElement('h1', null, props.title)
 );
 
 ReactDOM.render(
   React.createElement('div', null, [
     React.createElement('h2', null, 'welcome to'),
-    React.createElement(Header)
+    React.createElement(Header, {title: 'React - basic to advanced course'})
   ]),
   document.getElementById('root')
 );
