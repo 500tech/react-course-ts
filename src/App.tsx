10e5368e83bf7ba5da91bdab317a5afe93c0c57a
Shay Keinan <>
2018-05-15 23:54:16 +0300

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 
 class App extends React.Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      throw new Error('An error!');
+    }, 3000);
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
   render() {
+    const { hasError } = this.state;
+
     return (
       <div className="App">
-        hello world!
+        {hasError ? 'ERROR!' : 'hello world!'}
       </div>
     );
   }
