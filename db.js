99e90700d434233eef6fbc96084ff6743187f135
Maayan Glikser <maayan@glikm.com>
2018-05-28 15:06:12 +0300

    Setup for graphql

---
+++ db.js
@@ -0,0 +1,17 @@
+module.exports = {
+  movies: [
+    { id: 0, label: 'forest gump', rating: 1, description: 'Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny (Robin Wright).' },
+    { id: 1, label: 'wonder woman', rating: 2, description: 'Before she was Wonder Woman (Gal Gadot), she was Diana, princess of the Amazons, trained to be an unconquerable warrior. Raised on a sheltered island paradise, Diana meets an American pilot (Chris Pine) who tells her about the massive conflict that\'s raging in the outside world. Convinced that she can stop the threat, Diana leaves her home for the first time. Fighting alongside men in a war to end all wars, she finally discovers her full powers and true destiny.' },
+    { id: 2, label: 'a quiet place', rating: 1, description: 'If they hear you, they hunt you.' }
+  ],
+  actors: [
+    { id: 0, label: 'Tom hanks', movie_id: 0 },
+    { id: 1, label: 'Gal gadot', movie_id: 1 },
+    { id: 2, label: 'John Krasinski', movie_id: 3 }
+  ],
+  directors: [
+    { id: 0, label: 'Robert Zemeckis', movie_id: 0 },
+    { id: 1, label: 'Patty Jenkins', movie_id: 1 },
+    { id: 2, label: 'John Krasinski', movie_id: 2 },
+  ]
+};
