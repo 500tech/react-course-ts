2f0f17121b364f2fde98c52e425c4dca810cff88
Alon Niv <alon@500tech.com>
2018-12-30 15:09:21 +0200

    solution for size props

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// @TODO what if we want to control the size of the greeting?
-function Greeting({ name = 'stranger' }) {
+const GREETING_SIZES = {
+  TITLE: 'h1',
+  BIG: 'h2',
+  MEDIUM: 'h3',
+  SMALL: 'h5',
+};
+
+function Greeting({ name = 'stranger', size = GREETING_SIZES.TITLE }) {
   return React.createElement(
-    'h1',
+    size,
     { className: 'greeting' },
     `Hello, ${name}!`
   );
 }
 
+Object.assign(Greeting, GREETING_SIZES);
+
 ReactDOM.render(
   React.createElement('div', null, [
     // child components
-    React.createElement(Greeting, { name: 'foobar' }),
+    React.createElement(Greeting, { name: 'foobar', size: Greeting.MEDIUM }),
   ]),
   document.getElementById('root')
 );
