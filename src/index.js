c562ef791ec58cc41bad5f716c9982973b2e0557
Alon Niv <alon@500tech.com>
2018-12-31 12:03:32 +0200

    base flux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import runAll from './es6';
+/**
+ * Flux:
+ *
+ * UI -[creates]-> action -[through]-> dispatcher -[affects]-> state -[affects]-> UI
+ *
+ * For example:
+ * Todo List -[onClick]-> TOGGLE_TODO_ITEM -> (...) -> state = !state -> Todo list updates
+ */
 
-runAll();
+function createStore(state, handleAction) {
+  const stateSubscribers = new Set();
+  return {
+    dispatch(action) {
+      state = handleAction(state, action);
+      for (let sub of stateSubscribers) {
+        sub(state);
+      }
+    },
+    subscribe(cb) {
+      stateSubscribers.add(cb);
+      return () => stateSubscribers.delete(cb);
+    },
+  };
+}
+
+const store = createStore(0, function handleState(state, action) {
+  switch (action) {
+    case 'INCREMENT': {
+      return state + 1;
+    }
+    default: {
+      return state;
+    }
+  }
+});
+
+const counter = document.getElementById('counter');
+// @TODO create a decrement button
+// @TODO create a +n button that adds a random number (0-5)
+const incrementButton = document.getElementById('increment');
+
+store.subscribe(state => (counter.textContent = state));
+incrementButton.onclick = () => store.dispatch('INCREMENT');
+store.dispatch();
