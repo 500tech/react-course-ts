09b4009bd5c642b1e0bb42b03d898d6bc15ad404
oakfang <oakfang@gmail.com>
2019-01-01 09:57:42 +0200

    implement api mw

--- src/store/middleware/api.middleware.js
+++ src/store/middleware/api.middleware.js
@@ -1,1 +1,40 @@
-// @TODO implement api middleware
+import { NET_FAILURE, NET_SUCCESS } from '../utils';
+
+export default store => next => async action => {
+  next(action);
+  if (action.api) {
+    const {
+      url,
+      method = 'GET',
+      body,
+      headers = {},
+      successAction = NET_SUCCESS,
+      failureAction = NET_FAILURE,
+    } = action.api;
+    const fetchOptions = {
+      method,
+    };
+    if (body !== undefined) {
+      headers['Content-Type'] = 'application/json';
+      fetchOptions.body = JSON.stringify(body);
+    }
+    fetchOptions.headers = headers;
+    try {
+      const response = await fetch(url, fetchOptions);
+      if (!response.ok) {
+        throw await response.json();
+      }
+      store.dispatch({
+        type: successAction,
+        payload: await response.json(),
+        originalPayload: action.payload,
+      });
+    } catch (e) {
+      store.dispatch({
+        type: failureAction,
+        payload: e,
+        originalPayload: action.payload,
+      });
+    }
+  }
+};
--- src/store/utils.js
+++ src/store/utils.js
@@ -1,1 +1,10 @@
-export const createAction = type => payload => ({ type, payload });
+export const NET_SUCCESS = 'NET_SUCCESS';
+export const NET_FAILURE = 'NET_FAILURE';
+
+export const createAction = (type, api) => payload => {
+  const action = { type, payload };
+  if (api) {
+    action.api = typeof api === 'function' ? api(payload) : api;
+  }
+  return action;
+};
