6aece1b70de5089f52117625a77fcf9fc4e9b4a2
oakfang <oakfang@gmail.com>
2019-01-13 11:40:02 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import runAll from './es6';
 
 class User {
-  constructor() {}
+  constructor({ fullName, company }) {
+    this.fullName = fullName;
+    this.company = company;
+  }
 
-  getCompanyEmail(companyUsers) {}
+  _getBaseUsername() {
+    return this.fullName.replace(/ /g, '').replace('.', '');
+  }
+
+  getCompanyEmail(companyUsers) {
+    const base = this._getBaseUsername();
+    const normalisedCompany = this.company.replace(/ /g, '');
+    const similarNames = companyUsers.filter(
+      user => user._getBaseUsername() === base
+    );
+    const suffix = similarNames.length === 0 ? '' : similarNames.length;
+    return `${base}${suffix}@${normalisedCompany}.com`;
+  }
 }
 
 runAll();
 
-const user = new User({ fullName: 'Jogn F. Kennedy', company: 'White House' });
-user.getCompanyEmail([
-  //...
-])
+const user = new User({ fullName: 'John F. Kennedy', company: 'White House' });
+console.log(
+  user.getCompanyEmail([
+    new User({ fullName: 'John F. Kennedy', company: 'White House' }),
+    new User({ fullName: 'John III F. Kennedy', company: 'White House' }),
+    new User({ fullName: 'Johnny Kennedy', company: 'White House' }),
+  ])
+);
