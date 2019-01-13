35e4afd1763d06ca8bc93e62e4efeb8e0e0e1a67
oakfang <oakfang@gmail.com>
2019-01-13 10:28:31 +0200

    first exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import runAll from './es6';
 
+class User {
+  constructor() {}
+
+  getCompanyEmail(companyUsers) {}
+}
+
 runAll();
+
+const user = new User({ fullName: 'Jogn F. Kennedy', company: 'White House' });
+user.getCompanyEmail([
+  //...
+])
