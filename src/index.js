ed1b5037488dbc190bf059152fb3d0d60ee61644
oakfang <oakfang@gmail.com>
2019-01-14 10:28:36 +0200

    ex 3

--- src/index.js
+++ src/index.js
@@ -2,21 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/**
-<div id="root">
-  <div class="app-container">
-    <header>
-      <h1>Foobar</h1>
-    </header>
-    <main>
-      <h1 class="greeting">Hello, stranger!</h1>
-    </main>
-  </div>
-</div>
- */
-
-function App() {
-  return null;
+function Greeting({ name = 'stranger' }) {
+  return <h1 className="greeting">Hello, {name}!</h1>;
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// title can be a string (and, therefore, the actual title), or it can use
+// the children prop, but not both!
+function Header({ title, children }) {
+  if (children && title) {
+    console.error(
+      "'Header' component can't use both 'title' and 'children' props!"
+    );
+    // don't render anything
+    return null;
+  }
+  return <header>{title ? <h1>{title}</h1> : children}</header>;
+}
+
+function AppContainer({ children }) {
+  return <div className="app-container">{children}</div>;
+}
+
+/**
+ * @TODO if user exists, greet him/her (use Greeting)
+ * otherwise, just render children
+ */
+function App({ children }) {
+  return (
+    <AppContainer>
+      <Header title="foobar" />
+      <main>
+        <Greeting />
+        {children}
+      </main>
+    </AppContainer>
+  );
+}
+
+/**
+ * interface User {
+ *   name: string;
+ * }
+ */
+const user = null;
+
+ReactDOM.render(<App user={user} />, document.getElementById('root'));
