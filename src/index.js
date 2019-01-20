22b829507de7c51fb16488e9381216271536fb08
oakfang <oakfang@gmail.com>
2019-01-20 10:48:04 +0200

    stage 2

--- src/index.js
+++ src/index.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Counter() {
-  return null;
+function Counter({ count }) {
+  return <h1>{count}</h1>;
 }
 
-function App() {
-  return <div>
-    <Counter count={0} />
-    <button>+</button>
-    <button>-</button>
-  </div>
+function decrementCount(currentCount) {
+  console.log(currentCount - 1);
+}
+
+class App extends Component {
+  state = {
+    counter: {
+      count: 0,
+    },
+  };
+
+  incrementCount = () => {
+    const { counter } = this.state;
+    const newCounter = {
+      ...counter,
+      count: counter.count + 1,
+    };
+    this.setState({
+      counter: newCounter,
+    });
+  };
+
+  render() {
+    const { count } = this.state.counter;
+    return (
+      <div>
+        <Counter count={count} />
+        <button onClick={this.incrementCount}>+</button>
+        <button onClick={() => decrementCount(count)}>-</button>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
