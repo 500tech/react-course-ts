d1a2b79682ccc26da1a10028afca7f9eee6fc9bd
oakfang <oakfang@gmail.com>
2019-01-22 12:08:41 +0200

    Stage 5

--- src/index.js
+++ src/index.js
@@ -1,54 +1,37 @@
-/**
- * Flux:
- *
- * UI -[creates]-> action -[through]-> dispatcher -[affects]-> state -[affects]-> UI
- *
- * For example:
- * Todo List -[onClick]-> TOGGLE_TODO_ITEM -> (...) -> state = !state -> Todo list updates
- */
+import { createStore, combineReducers } from 'redux';
 
-const compose = (...fns) => arg => {
-  for (let fn of fns) {
-    arg = fn(arg);
-  }
-  return arg;
-};
-
-function createStore(initialState, stateReducer, middlewares) {
-  stateReducer = middlewares
-    ? compose(...middlewares)(stateReducer)
-    : stateReducer;
-  const subscribers = new Set();
-  let state = initialState;
-
-  function notify() {
-    for (let sub of subscribers) {
-      sub(state);
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      const { payload = 1 } = action;
+      return state + payload;
+    }
+    case 'DECREMENT': {
+      return state - 1;
+    }
+    default: {
+      return state;
     }
   }
+};
 
-  return {
-    getState() {
+const userReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'LOGIN': {
+      return action.payload;
+    }
+    default: {
       return state;
-    },
-    subscribe(cb) {
-      subscribers.add(cb);
-      return () => subscribers.delete(cb);
-    },
-    dispatch(action, force) {
-      const newState = stateReducer(state, action);
-      if (newState !== state || force) {
-        state = newState;
-        notify();
-      }
-    },
-  };
-}
+    }
+  }
+};
 
-function createDispatcher(...stores) {
-  return (action, force) =>
-    stores.forEach(store => store.dispatch(action, force));
-}
+const store = createStore(
+  combineReducers({
+    count: countReducer,
+    user: userReducer,
+  })
+);
 
 function mapperMiddleware(stateMapper) {
   return stateReducer => (state, action) =>
@@ -65,27 +48,6 @@ function loggerMiddleware(reducer) {
   };
 }
 
-const countStore = createStore(
-  0,
-  (state, action) => {
-    switch (action.type) {
-      case 'INCREMENT': {
-        const { payload = 1 } = action;
-        return state + payload;
-      }
-      case 'DECREMENT': {
-        return state - 1;
-      }
-      default: {
-        return state;
-      }
-    }
-  },
-  [loggerMiddleware, mapperMiddleware(state => Math.max(state, 0))]
-);
-
-const dispatch = createDispatcher(countStore);
-
 const counter = document.getElementById('counter');
 const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
@@ -98,9 +60,16 @@ function getRandom() {
 function increment(incrementBy = 1) {
   return { type: 'INCREMENT', payload: incrementBy };
 }
-countStore.subscribe(state => (counter.textContent = state));
-incrementButton.onclick = () => dispatch({ type: 'INCREMENT' });
-decrementButton.onclick = () => dispatch({ type: 'DECREMENT' });
-randomButton.onclick = () => dispatch(increment(getRandom()));
-countStore.subscribe(state => (decrementButton.disabled = state === 0));
-dispatch({ type: 'INIT' }, true);
+store.subscribe(() => {
+  const state = store.getState();
+  counter.textContent = state.count;
+  decrementButton.disabled = state.count === 0;
+  if (state.user) {
+    document.getElementById('user').textContent = state.user;
+  }
+});
+incrementButton.onclick = () => store.dispatch({ type: 'INCREMENT' });
+decrementButton.onclick = () => store.dispatch({ type: 'DECREMENT' });
+randomButton.onclick = () => store.dispatch(increment(getRandom()));
+store.dispatch({ type: 'INIT' });
+setTimeout(() => store.dispatch({ type: 'LOGIN', payload: 'foobar' }), 5000);
