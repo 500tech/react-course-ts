bbd95ceaa1d8fc5a76a19545dc75805d7b47a682
oakfang <oakfang@gmail.com>
2019-02-03 18:20:06 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/**
- * Create Counter Application.
- * It should start at 0 and have a + and a -
- * button to change the count.
- */
+function Count({ count }) {
+  return <p>{count}</p>;
+}
+
 class Counter extends Component {
-  
+  state = { count: 0 };
+  changeCountBy = changeBy => () =>
+    this.setState({
+      count: this.state.count + changeBy,
+    });
+  render() {
+    return (
+      <div>
+        <button
+          disabled={this.state.count === 0}
+          onClick={this.changeCountBy(-1)}
+        >
+          -
+        </button>
+        <Count count={this.state.count} />
+        <button onClick={this.changeCountBy(+1)}>+</button>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
