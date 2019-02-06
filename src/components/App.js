77230c863cf9de366308fc0c225aebd2cb40a340
oakfang <oakfang@gmail.com>
2019-02-06 13:26:16 +0200

    refactor App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import uuid from 'uuid';
+import produce from 'immer';
 
 import { AddTodo } from './AddTodo';
 import { Greeting } from './Greeting';
@@ -10,45 +11,35 @@ const Container = styled.div`
   border: 1px solid lightblue;
 `;
 
-export class App extends Component {
-  state = {
-    todos: [
-      { $id: uuid(), text: 'This is todo #1', done: true },
-      { $id: uuid(), text: 'This is todo #2', done: false },
-      { $id: uuid(), text: 'This is todo #3', done: false },
-      { $id: uuid(), text: 'This is todo #4', done: false },
-      { $id: uuid(), text: 'This is todo #5', done: false },
-    ],
-  };
+const INITIAL_STATE = [
+  { $id: uuid(), text: 'This is todo #1', done: true },
+  { $id: uuid(), text: 'This is todo #2', done: false },
+  { $id: uuid(), text: 'This is todo #3', done: false },
+  { $id: uuid(), text: 'This is todo #4', done: false },
+  { $id: uuid(), text: 'This is todo #5', done: false },
+];
 
-  toggleTodo = todoIndex => {
-    const { todos } = this.state;
-    const newTodos = todos.map((todo, idx) =>
-      idx === todoIndex
-        ? {
-            ...todo,
-            done: !todo.done,
-          }
-        : todo
+const getTodo = text => ({ text, done: false, $id: uuid() });
+
+export function App() {
+  const [todos, setTodos] = useState(INITIAL_STATE);
+  const toggleTodo = todoIndex =>
+    setTodos(
+      produce(todos, draftState => {
+        draftState[todoIndex].done = !draftState[todoIndex].done;
+      })
     );
-    this.setState({ todos: newTodos });
-  };
-
-  onAddTodo = text => {
-    const todo = { text, done: false, $id: uuid() };
-    const { todos } = this.state;
-    this.setState({
-      todos: [...todos, todo],
-    });
-  };
-
-  render() {
-    return (
-      <Container>
-        <Greeting name="foobar" />
-        <AddTodo onAddTodo={this.onAddTodo} />
-        <TodoList todos={this.state.todos} toggleTodo={this.toggleTodo} />
-      </Container>
+  const onAddTodo = text =>
+    setTodos(
+      produce(todos, draftState => {
+        draftState.push(getTodo(text));
+      })
     );
-  }
+  return (
+    <Container>
+      <Greeting name="foobar" />
+      <AddTodo onAddTodo={onAddTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+    </Container>
+  );
 }
