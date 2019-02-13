85afaa424c71d3ea666f15cd2f0884c372fe0371
oakfang <oakfang@gmail.com>
2019-02-13 11:27:27 +0200

    redux init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * interface Action {
+ *  type: string;
+ *  payload?: jsonable;
+ *  meta?: jsonable;
+ *  error?: jsonable;
+ * }
+ */
+
+function count(state = 0, action) {
+  switch (action.type) {
+    case 'INCEREMENT':
+      return state + 1;
+    case 'DECEREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+function clicks(state = 0, action) {
+  switch (action.type) {
+    case 'INCEREMENT':
+    case 'DECEREMENT':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+// function master(state = {}, action) {
+//   return {
+//     count: count(state.count, action),
+//     clicks: clicks(state.clicks, action),
+//   };
+// }
+
+function combineReducers(keyToReducerMap) {
+  return function(state = {}, action) {
+    ///
+  };
+}
+
+const store = createStore(
+  combineReducers({
+    count,
+    clicks,
+  })
+);
+console.log(store.getState());
+store.subscribe(() => console.log(store.getState()));
+store.dispatch({ type: 'INCEREMENT' });
+store.dispatch({ type: 'INCEREMENT' });
+store.dispatch({ type: 'DECEREMENT' });
+store.dispatch({ type: 'RANDOMALIT' });
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { App } from './components';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
