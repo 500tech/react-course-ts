c82de09c66077a38a458d57ff85d71a2c17f50ec
Alon Niv <oakfang@gmail.com>
2019-02-17 14:15:26 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function Title({ children }) {
-  return (
-    <h1 style={{ background: 'red' }}>
-      {children}
-      <p>{children}</p>
-    </h1>
-  );
+const todos = [
+  { text: 'Foobar meow', done: true },
+  { text: 'Spam buzz pow', done: false },
+  { text: 'Find better things to do', done: false },
+];
+
+const Todo = ({ todo }) => (
+  <p style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+    {todo.text}
+  </p>
+);
+
+const TodoList = () => (
+  <div>
+    <Todo todo={todos[0]} />
+    <Todo todo={todos[1]} />
+    <Todo todo={todos[2]} />
+  </div>
+);
+
+function Title({ title, children, hidden }) {
+  if (hidden) {
+    return null;
+  }
+  return <h1 style={{ background: 'red' }}>{title ? title : children}</h1>;
 }
+
+const Foo = () => <span>Foo</span>;
 
 render(
   <div>
-    <Title>
+    <Title title="meow" hidden>
       <hr />
-      <span>Foo</span>
+      <Foo />
     </Title>
+    <TodoList />
   </div>,
   document.getElementById('root')
 );
