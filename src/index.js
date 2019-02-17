33f65293937f9efc8eab8301fce2735f1ba80192
Alon Niv <oakfang@gmail.com>
2019-02-17 13:32:53 +0200

    jsx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import runAll from './es6';
+import React from 'react';
+import { render } from 'react-dom';
 
-runAll();
+function Title({ children }) {
+  return (
+    <h1 style={{ background: 'red' }}>
+      {children}
+      <p>{children}</p>
+    </h1>
+  );
+}
+
+render(
+  <div>
+    <Title>
+      <hr />
+      <span>Foo</span>
+    </Title>
+  </div>,
+  document.getElementById('root')
+);
