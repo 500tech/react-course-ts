7327fcd0225510e020bc1dd3017f384474d71ba6
Alon Niv <oakfang@gmail.com>
2019-02-18 11:54:36 +0200

    styled

--- src/components/common/Foo.js
+++ src/components/common/Foo.js
@@ -1,3 +1,25 @@
 import React, { memo } from 'react';
+import styled from 'styled-components';
 
-export const Foo = memo(() => console.log('render') || <p>Foo</p>);
+// function pstyled(Component) {
+//   return (rawCss) => {
+//     return props => {
+//       const css = cssFactory(rawCss, props);
+//       const className = genrateNewClassNameFromCSS(css);
+//       return React.createElement(Component, {
+//         ...props,
+//         className: `${className} ${props.className}`,
+//       }, props.children);
+//     }
+//   };
+// }
+
+const BaseFoo = memo(({ className }) => <p className={className}>Foo</p>);
+
+export const Foo = styled(BaseFoo)`
+  color: red;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`;
