88796c6575d9cdb798b03328c761ddac50baab04
Alon Niv <oakfang@gmail.com>
2019-02-19 15:53:21 +0200

    final

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from './components/App';
 import store from './state';
+/**
+- create all todos actions as redux action (consts, action creators)
+- create todos reducer for redux and use it
+- remove all internal state references to todos (App, context)
+- connect Todos page with redux
+- make sure nothing is broken :)
+ */
 
 render(
   <Router>
