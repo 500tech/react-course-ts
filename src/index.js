d6a35b8cee4f46bc9c9f5f9ee73e7cda4b8e6fa3
Alon Niv <oakfang@gmail.com>
2019-02-19 16:26:03 +0200

    feedback link

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from './components/App';
 import store from './state';
+
+//feedback: https://docs.google.com/forms/d/e/1FAIpQLScWBhMSw3iC7Js_zoES3g2av8ANkboybMo6yi6enxZqJnCHwg/viewform
 /**
 - create all todos actions as redux action (consts, action creators)
 - create todos reducer for redux and use it
