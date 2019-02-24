b3791c7dacd4308cf0790fe2508302a9a5a92178
oakfang <oakfang@gmail.com>
2019-02-24 16:39:33 +0200

    dev tools

--- src/state/index.js
+++ src/state/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import mainReducer from './reducers';
 import mw from './middleware';
 
-export const store = createStore(mainReducer, mw);
+export const store = createStore(mainReducer, composeWithDevTools(mw));
