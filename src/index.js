b8f61e203e4f957b60fe7fdfca3fa12a5d9378d3
oakfang <oakfang@gmail.com>
2019-02-25 14:51:11 +0200

    feedback form link

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { App } from './components/App';
 import { store } from './state';
-
+// Feedback form: 
+// http://bit.ly/2BRGijH
 render(
   <Router>
     <ReduxProvider store={store}>
