80fe3df106ade3736712ea0dd423549fd5051273
oakfang <oakfang@gmail.com>
2019-06-17 10:39:15 +0300

    Cleanup files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="App-logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
