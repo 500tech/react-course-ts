aa6d6fb5646ea62535891fc626726b28e8de82f5
oakfang <oakfang@gmail.com>
2019-07-21 11:53:38 +0300

    items

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuid from 'uuid';
+import './index.css';
 
-function App({ color = 'pink', children }) {
+// Todo { text, done, id }
+
+function App({ color = 'pink', items = [], children }) {
   return (
-    <div>
-      <h1 style={{ color }}>Hello, world!</h1>
-      <p>{children}</p>
+    <div className="container">
+      <h1 style={{ color }}>Todo list</h1>
+      {children ? <p>{children}</p> : null}
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
+const TODOS = [
+  { id: uuid(), text: 'Learn Hebrew', done: false },
+  { id: uuid(), text: 'Order lunch', done: true },
+];
+
 ReactDOM.render(
-  <App color="blue">
+  <App color="blue" items={TODOS}>
     Display this
   </App>,
   document.getElementById('root')
