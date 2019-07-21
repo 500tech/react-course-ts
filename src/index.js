1a50ba0bcd6dc7e9a203d13982ac824a76fecde6
oakfang <oakfang@gmail.com>
2019-07-21 13:52:57 +0300

    events

--- src/index.js
+++ src/index.js
@@ -5,16 +5,46 @@ import './index.css';
 
 // Todo { text, done, id }
 
+function NoItemsEmptyState() {
+  return <p>Oh noes, no items yet! Please create one :)</p>;
+}
+
+function Todo({ todo }) {
+  const style = { textDecoration: todo.done ? 'line-through' : 'none' };
+  return (
+    <li
+      style={style}
+      onClick={event => {
+        // event.persist();
+        const { target } = event;
+        console.log(event);
+        console.log(event.target);
+        setTimeout(() => {
+          console.log(target);
+        }, 3);
+      }}
+    >
+      {todo.text}
+    </li>
+  );
+}
+
+function TodoList({ items }) {
+  return (
+    <ul>
+      {items.map(item => (
+        <Todo key={item.id} todo={item} />
+      ))}
+    </ul>
+  );
+}
+
 function App({ color = 'pink', items = [], children }) {
   return (
     <div className="container">
       <h1 style={{ color }}>Todo list</h1>
       {children ? <p>{children}</p> : null}
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
+      {items.length ? <TodoList items={items} /> : <NoItemsEmptyState />}
     </div>
   );
 }
@@ -23,6 +53,8 @@ const TODOS = [
   { id: uuid(), text: 'Learn Hebrew', done: false },
   { id: uuid(), text: 'Order lunch', done: true },
 ];
+
+window.todos = TODOS;
 
 ReactDOM.render(
   <App color="blue" items={TODOS}>
