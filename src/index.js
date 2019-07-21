fc80f1a2da803df517f0a1e03418f684776199f0
oakfang <oakfang@gmail.com>
2019-07-21 15:39:02 +0300

    service

--- src/index.js
+++ src/index.js
@@ -11,35 +11,38 @@ function NoItemsEmptyState() {
   return <p>Oh noes, no items yet! Please create one :)</p>;
 }
 
-function Todo({ todo, onToggleTodo = NOOP }) {
+function Todo({ todo, onToggleTodo = NOOP, onRemoveTodo = NOOP }) {
   const style = { textDecoration: todo.done ? 'line-through' : 'none' };
   return (
-    <li style={style} onClick={() => onToggleTodo(todo)}>
+    <li
+      style={style}
+      onClick={() => onToggleTodo(todo)}
+      onDoubleClick={() => onRemoveTodo(todo)}
+    >
       {todo.text}
     </li>
   );
 }
 
-function TodoList({ items, onToggleTodo }) {
+function TodoList({ items, onToggleTodo, onRemoveTodo }) {
   return (
     <ul>
       {items.map(item => (
-        <Todo key={item.id} todo={item} onToggleTodo={onToggleTodo} />
+        <Todo
+          key={item.id}
+          todo={item}
+          onToggleTodo={onToggleTodo}
+          onRemoveTodo={onRemoveTodo}
+        />
       ))}
     </ul>
   );
 }
 
-function App({ children }) {
-  const [todos, setTodos] = useState([
-    { id: uuid(), text: 'Learn Hebrew', done: false },
-    { id: uuid(), text: 'Order lunch', done: true },
-  ]);
-  const [color, setColor] = useState('blue');
+function useTodosService(initialTodos) {
+  const [todos, setTodos] = useState(initialTodos);
 
   const toggleTodo = todo => {
-    // todo.done = !todo.done;
-    // setTodos([...todos]);
     return setTodos(
       todos.map(t =>
         t.id === todo.id
@@ -51,12 +54,29 @@ function App({ children }) {
       )
     );
   };
+
+  const removeTodo = todo => setTodos(todos.filter(({ id }) => id !== todo.id));
+
+  return { todos, toggleTodo, removeTodo };
+}
+
+function App({ children }) {
+  const { todos, removeTodo, toggleTodo } = useTodosService([
+    { id: uuid(), text: 'Learn Hebrew', done: false },
+    { id: uuid(), text: 'Order lunch', done: true },
+  ]);
+  const [color, setColor] = useState('blue');
+
   return (
     <div className="container" onClick={() => setColor('red')}>
       <h1 style={{ color }}>Todo list</h1>
       {children ? <p>{children}</p> : null}
       {todos.length ? (
-        <TodoList items={todos} onToggleTodo={toggleTodo} />
+        <TodoList
+          items={todos}
+          onToggleTodo={toggleTodo}
+          onRemoveTodo={removeTodo}
+        />
       ) : (
         <NoItemsEmptyState />
       )}
