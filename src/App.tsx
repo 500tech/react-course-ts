73ec1fa3dd44b2e5c78d1c4ce6bf8892b8ea0f05
oakfang <oakfang@gmail.com>
2019-07-22 10:12:58 +0300

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from "react";
-import { ReactComponent as Logo } from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
-const App: React.FC = () => {
+function useCount() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => setCount(count => count + 1);
+  return { count, incrementCount };
+}
+
+const Counter: React.FC = () => {
+  console.log("render");
+  const [clicks, setClicks] = useState(0);
+  const { count, incrementCount } = useCount();
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      onClick={e => {
+        console.log(e.target);
+        setTimeout(() => {
+          console.log(e.target);
+          setClicks(clicks + 1);
+          incrementCount();
+          incrementCount();
+        });
+      }}
+    >
+      {clicks} {count}
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  const { count, incrementCount } = useCount();
+  return (
+    <div className="App" onDoubleClick={incrementCount}>
+      <Counter />
+      {count}
     </div>
   );
 };
