6f594f9d68285b1b3225e736f2402398cc3b690c
oakfang <oakfang@gmail.com>
2019-07-22 10:46:29 +0300

    setup todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,52 @@
 import React, { useState } from "react";
-import "./App.css";
+import uuid from "uuid";
+import "./index.css";
 
-function useCount() {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => setCount(count => count + 1);
-  return { count, incrementCount };
+interface Todo {
+  text: string;
+  id: string;
+  done: boolean;
 }
 
-const Counter: React.FC = () => {
-  console.log("render");
-  const [clicks, setClicks] = useState(0);
-  const { count, incrementCount } = useCount();
+function NoItemsEmptyState() {
+  return <p>Oh noes, no items yet! Please create one :)</p>;
+}
+
+interface TodoProps {
+  todo: Todo;
+}
+
+const TodoItem: React.FC<TodoProps> = ({ todo }) => {
+  const style = { textDecoration: todo.done ? "line-through" : "none" };
+  return <li style={style}>{todo.text}</li>;
+};
+
+interface TodoListProps {
+  items: Todo[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ items }) => {
   return (
-    <div
-      onClick={e => {
-        console.log(e.target);
-        setTimeout(() => {
-          console.log(e.target);
-          setClicks(clicks + 1);
-          incrementCount();
-          incrementCount();
-        });
-      }}
-    >
-      {clicks} {count}
-    </div>
+    <ul>
+      {items.map(item => (
+        <TodoItem key={item.id} todo={item} />
+      ))}
+    </ul>
   );
 };
 
-const App: React.FC = () => {
-  const { count, incrementCount } = useCount();
+const App: React.FC = ({ children }) => {
+  const [todos] = useState<Todo[]>([
+    { id: uuid(), text: "Learn hooks", done: false },
+    { id: uuid(), text: "Order lunch", done: true }
+  ]);
+  const [color, setColor] = useState("blue");
+
   return (
-    <div className="App" onDoubleClick={incrementCount}>
-      <Counter />
-      {count}
+    <div className="container" onClick={() => setColor("red")}>
+      <h1 style={{ color }}>Todo list</h1>
+      {children ? <p>{children}</p> : null}
+      {todos.length ? <TodoList items={todos} /> : <NoItemsEmptyState />}
     </div>
   );
 };
