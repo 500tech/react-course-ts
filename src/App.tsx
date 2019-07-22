b5059428e82255760b1cf5053579b9af91f8255c
oakfang <oakfang@gmail.com>
2019-07-22 12:02:55 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import uuid from "uuid";
 import "./index.css";
 
@@ -17,12 +17,23 @@ const NOOP = () => null;
 interface TodoProps {
   todo: Todo;
   onToggleTodo?: (todo: Todo) => void;
+  onRemoveTodo?: (todo: Todo) => void;
 }
 
-const TodoItem: React.FC<TodoProps> = ({ todo, onToggleTodo = NOOP }) => {
+const TodoItem: React.FC<TodoProps> = ({
+  todo,
+  onToggleTodo = NOOP,
+  onRemoveTodo = NOOP
+}) => {
+  console.log("render");
   const style = { textDecoration: todo.done ? "line-through" : "none" };
   return (
-    <li style={style} onClick={() => onToggleTodo(todo)}>
+    <li
+      style={style}
+      onClick={e =>
+        e.metaKey || e.ctrlKey ? onRemoveTodo(todo) : onToggleTodo(todo)
+      }
+    >
       {todo.text}
     </li>
   );
@@ -31,27 +42,31 @@ const TodoItem: React.FC<TodoProps> = ({ todo, onToggleTodo = NOOP }) => {
 interface TodoListProps {
   items: Todo[];
   onToggleTodo?: (todo: Todo) => void;
+  onRemoveTodo?: (todo: Todo) => void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({ items, onToggleTodo }) => {
+const TodoList: React.FC<TodoListProps> = ({
+  items,
+  onToggleTodo,
+  onRemoveTodo
+}) => {
   return (
     <ul>
       {items.map(item => (
-        <TodoItem key={item.id} todo={item} onToggleTodo={onToggleTodo} />
+        <TodoItem
+          key={item.id}
+          todo={item}
+          onToggleTodo={onToggleTodo}
+          onRemoveTodo={onRemoveTodo}
+        />
       ))}
     </ul>
   );
 };
 
-const App: React.FC = ({ children }) => {
-  const [todos, setTodos] = useState<Todo[]>([
-    { id: uuid(), text: "Learn hooks", done: false },
-    { id: uuid(), text: "Eat breakfast", done: true }
-  ]);
-  const [color, setColor] = useState<"blue" | "red">("blue");
-  const toggleTodo = (todo: Todo) => {
-    // todo.done = !todo.done;
-    // setTodos([...todos]);
+function useTodosService(initialTodos: Todo[]) {
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const toggleTodo = useCallback((todo: Todo) => {
     setTodos(todos =>
       todos.map(t =>
         t.id === todo.id
@@ -62,14 +77,33 @@ const App: React.FC = ({ children }) => {
           : t
       )
     );
-  };
+  }, []);
+
+  const removeTodo = useCallback(
+    (todo: Todo) => setTodos(todos => todos.filter(t => t.id !== todo.id)),
+    []
+  );
+
+  return { todos, toggleTodo, removeTodo };
+}
+
+const App: React.FC = ({ children }) => {
+  const { todos, toggleTodo, removeTodo } = useTodosService([
+    { id: uuid(), text: "Learn hooks", done: false },
+    { id: uuid(), text: "Eat breakfast", done: true }
+  ]);
+  const [color, setColor] = useState<"blue" | "red">("blue");
 
   return (
     <div className="container" onClick={() => setColor("red")}>
       <h1 style={{ color }}>Todo list</h1>
       {children ? <p>{children}</p> : null}
       {todos.length ? (
-        <TodoList items={todos} onToggleTodo={toggleTodo} />
+        <TodoList
+          items={todos}
+          onToggleTodo={toggleTodo}
+          onRemoveTodo={removeTodo}
+        />
       ) : (
         <NoItemsEmptyState />
       )}
