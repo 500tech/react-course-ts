61c35a9cb4275c06d06d3b6f0d3417959e09c291
oakfang <oakfang@gmail.com>
2019-07-22 11:25:47 +0300

    toggle

--- src/App.tsx
+++ src/App.tsx
@@ -12,41 +12,67 @@ function NoItemsEmptyState() {
   return <p>Oh noes, no items yet! Please create one :)</p>;
 }
 
+const NOOP = () => null;
+
 interface TodoProps {
   todo: Todo;
+  onToggleTodo?: (todo: Todo) => void;
 }
 
-const TodoItem: React.FC<TodoProps> = ({ todo }) => {
+const TodoItem: React.FC<TodoProps> = ({ todo, onToggleTodo = NOOP }) => {
   const style = { textDecoration: todo.done ? "line-through" : "none" };
-  return <li style={style}>{todo.text}</li>;
+  return (
+    <li style={style} onClick={() => onToggleTodo(todo)}>
+      {todo.text}
+    </li>
+  );
 };
 
 interface TodoListProps {
   items: Todo[];
+  onToggleTodo?: (todo: Todo) => void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({ items }) => {
+const TodoList: React.FC<TodoListProps> = ({ items, onToggleTodo }) => {
   return (
     <ul>
       {items.map(item => (
-        <TodoItem key={item.id} todo={item} />
+        <TodoItem key={item.id} todo={item} onToggleTodo={onToggleTodo} />
       ))}
     </ul>
   );
 };
 
 const App: React.FC = ({ children }) => {
-  const [todos] = useState<Todo[]>([
+  const [todos, setTodos] = useState<Todo[]>([
     { id: uuid(), text: "Learn hooks", done: false },
-    { id: uuid(), text: "Order lunch", done: true }
+    { id: uuid(), text: "Eat breakfast", done: true }
   ]);
-  const [color, setColor] = useState("blue");
+  const [color, setColor] = useState<"blue" | "red">("blue");
+  const toggleTodo = (todo: Todo) => {
+    // todo.done = !todo.done;
+    // setTodos([...todos]);
+    setTodos(todos =>
+      todos.map(t =>
+        t.id === todo.id
+          ? {
+              ...todo,
+              done: !todo.done
+            }
+          : t
+      )
+    );
+  };
 
   return (
     <div className="container" onClick={() => setColor("red")}>
       <h1 style={{ color }}>Todo list</h1>
       {children ? <p>{children}</p> : null}
-      {todos.length ? <TodoList items={todos} /> : <NoItemsEmptyState />}
+      {todos.length ? (
+        <TodoList items={todos} onToggleTodo={toggleTodo} />
+      ) : (
+        <NoItemsEmptyState />
+      )}
     </div>
   );
 };
