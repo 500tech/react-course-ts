4a8badb44399794d7b11e7f7cbc80f71826a0979
oakfang <oakfang@gmail.com>
2019-07-23 15:09:43 +0300

    react rollback

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import "./index.css";
-// import App from "./components/App";
-// import { TodosProvider } from "./components/TodosProvider";
-// import { RouterProvider } from "./components/RouterProvider";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
+import { TodosProvider } from "./components/TodosProvider";
+import { RouterProvider } from "./components/RouterProvider";
 
-// ReactDOM.render(
-//   <Router>
-//     <RouterProvider>
-//       <TodosProvider>
-//         <App />
-//       </TodosProvider>
-//     </RouterProvider>
-//   </Router>,
-//   document.getElementById("root")
-// );
-
-import './alternative';
+ReactDOM.render(
+  <Router>
+    <RouterProvider>
+      <TodosProvider>
+        <App />
+      </TodosProvider>
+    </RouterProvider>
+  </Router>,
+  document.getElementById("root")
+);
