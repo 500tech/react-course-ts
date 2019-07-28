0698dd49bfaae3af73107ab3e5bf800920cfabaf
oakfang <oakfang@gmail.com>
2019-07-28 14:09:36 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -15,17 +15,31 @@ const TODOS = [
   { id: getId(), title: 'Learn ReactJS', completed: false },
 ];
 
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+function EmptyState() {
+  return (
+    <h3>
+      Nothing to do! <code>goto</code> the beach!
+    </h3>
+  );
+}
+
 export function App({ greeting = 'Hello', username }) {
   return (
     <div className="container">
       <h1>
         <span>{username ? `${greeting}, ${username}` : greeting}</span>
       </h1>
-      <ul>
-        {TODOS.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      {TODOS && TODOS.length ? <TodoList todos={TODOS} /> : <EmptyState />}
     </div>
   );
 }
