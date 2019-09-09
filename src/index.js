59aabc0d846ca04437ec9c833f446c924b3c4b10
oakfang <oakfang@gmail.com>
2019-09-09 11:08:41 +0300

    init some code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
+function App({ color = 'blue', children }) {
+  return (
+    <div className="main-container">
+      <h1 style={{ color }}>Hello, world!</h1>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App>Yo yo!</App>, document.getElementById('root'));
+
+// React.createElement('div', null, [
+//   React.createElement('h1', { style: { color: 'blue' } }, ['Hello, world!']),
+//   React.createElement('p', null, ['Yo yo']),
+// ]);
