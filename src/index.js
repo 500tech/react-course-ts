2769be8c05502bda6be2bc8754e7551f791818b0
oakfang <oakfang@gmail.com>
2019-09-09 12:09:54 +0300

    for

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { getUniqueId } from './utils';
+
+/**
+ * interface Todo {
+ *  id: number;
+ *  title: string;
+ *  completed: boolean;
+ * }
+ */
+
+const TODOS = [
+  { id: getUniqueId(), title: 'Go home', completed: false },
+  { id: getUniqueId(), title: 'Order 10bis', completed: true },
+  { id: getUniqueId(), title: 'Take a break', completed: false },
+];
+
+window.TODOS = TODOS;
 
 function BorderedContainer({ children }) {
   return <div className="main-container">{children}</div>;
@@ -10,22 +27,42 @@ function Title({ color, children }) {
   return <h1 style={{ color }}>{children}</h1>;
 }
 
-function App({ color = 'blue', children }) {
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li
+          key={todo.id}
+          style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+        >
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function App({ color = 'blue', children, todos }) {
   return (
     <BorderedContainer>
       <Title color={color}>Hello, world!</Title>
       {children ? <p>{children}</p> : null}
       {children && <p>{children}</p>}
+      <TodoList todos={todos} />
     </BorderedContainer>
   );
 }
 
-ReactDOM.render(<App>Yo yo!</App>, document.getElementById('root'));
+ReactDOM.render(
+  <App todos={TODOS}>Yo yo!</App>,
+  document.getElementById('root')
+);
 
-// React.createElement('div', null, [
-//   React.createElement('h1', { style: { color: 'blue' } }, [
-//     'Hello, world!',
-//     0 ? 1 : 2
-//   ]),
-//   React.createElement('p', null, ['Yo yo']),
+// React.createElement(BorderedContainer, null, [
+//  React.createElement(Title, { color }, ['Hello world']),
+//  null,
+//  null,
+//  React.createElement(TodoList, { todos: todos }),
 // ]);
+
+// React.createElement('div', { className: 'sfsdf' })
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+let id = 0;
+
+export const getUniqueId = () => id++;
+
+export default 5;
