9395ae8da739694f38b29d9beb75725c7733ce31
oakfang <oakfang@gmail.com>
2019-09-09 11:31:04 +0300

    multiple components

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function BorderedContainer({ children }) {
+  return <div className="main-container">{children}</div>;
+}
+
+function Title({ color, children }) {
+  return <h1 style={{ color }}>{children}</h1>;
+}
+
 function App({ color = 'blue', children }) {
   return (
-    <div className="main-container">
-      <h1 style={{ color }}>Hello, world!</h1>
-      <p>{children}</p>
-    </div>
+    <BorderedContainer>
+      <Title color={color}>Hello, world!</Title>
+      {children ? <p>{children}</p> : null}
+      {children && <p>{children}</p>}
+    </BorderedContainer>
   );
 }
 
 ReactDOM.render(<App>Yo yo!</App>, document.getElementById('root'));
 
 // React.createElement('div', null, [
-//   React.createElement('h1', { style: { color: 'blue' } }, ['Hello, world!']),
+//   React.createElement('h1', { style: { color: 'blue' } }, [
+//     'Hello, world!',
+//     0 ? 1 : 2
+//   ]),
 //   React.createElement('p', null, ['Yo yo']),
 // ]);
