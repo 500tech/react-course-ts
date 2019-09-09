6d6c129ea20dbfe0f3d274780e8fa2c8e614d753
oakfang <oakfang@gmail.com>
2019-09-09 08:35:25 +0300

    init env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App({ color="pink" }) {
-  return (
-    <div>
-      <h1 style={{ color }}>Hello, world!</h1>
-      <p>Hello, {3 + 4}!</p>
-    </div>
-  );
-}
-
-ReactDOM.render(<App color="blue" />, document.getElementById('root'));
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
