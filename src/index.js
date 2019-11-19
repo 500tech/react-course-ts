91a4aa8ce490ebaa2f575a08c45b2043bdc1bf83
Alon Niv <oakfang@gmail.com>
2019-11-19 09:51:22 +0200

    title

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class Title extends Component {
+  render() {
+    const { children, color } = this.props;
+    return <h1 style={{ color, backgroundColor: 'yellow' }}>{children}</h1>;
+  }
+}
+
 class App extends Component {
   render() {
-    const { titleColor="blue" } = this.props;
+    const { titleColor = 'blue' } = this.props;
     return (
       <div>
-        <h1 style={{ color: titleColor, backgroundColor: 'yellow' }}>
+        {/* <h1 style={{ color: titleColor, backgroundColor: 'yellow' }}>
           Hello, world!
-        </h1>
+        </h1> */}
+        <Title color={titleColor}>Have a great evening!</Title>
         <p>Tagline</p>
       </div>
     );
