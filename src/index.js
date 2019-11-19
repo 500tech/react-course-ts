931db2e160f45c60ea57d536677a6d7bfa43e1d0
Alon Niv <oakfang@gmail.com>
2019-11-19 09:51:22 +0200

    map

--- src/index.js
+++ src/index.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const TODOS = [
+  { id: 0, title: 'Eat lunch', completed: true },
+  { id: 1, title: 'Drink third double espresso', completed: false },
+];
+
+class TodoItem extends Component {
+  render() {
+    const { todo } = this.props;
+    return (
+      <li style={todo.completed ? { textDecoration: 'line-through' } : {}}>
+        {todo.title}
+      </li>
+    );
+  }
+}
+
+class Title extends Component {
+  render() {
+    const { color, children } = this.props;
+    return <h1 style={{ color }}>{children}</h1>;
+  }
+}
+
 class App extends Component {
   render() {
     const { titleColor = 'blue' } = this.props;
     return (
       <div className="container">
-        <h1 style={{ color: titleColor }}>Hello, world!</h1>
-        {/* <Title color={titleColor} text="Hello, world!" /> */}
-        <p className="tagline">Tagline</p>
+        <Title color={titleColor}>Hello world!</Title>
+        <ul>
+          {TODOS.map(todo => (
+            <TodoItem todo={todo} />
+          ))}
+        </ul>
         <button>Click me!</button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App titleColor="red" />, document.querySelector('#root'));
+ReactDOM.render(<App titleColor="red"></App>, document.querySelector('#root'));
