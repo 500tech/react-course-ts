112892b26e9eea6378d513b0b35bc1322efa1aa9
Alon Niv <oakfang@gmail.com>
2019-11-19 09:51:22 +0200

    todos state

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ const TODOS = [
   { id: 1, title: 'Drink third double espresso', completed: false },
 ];
 
+window.TODOS = TODOS;
+
 class TodoItem extends Component {
   render() {
-    const { todo } = this.props;
+    const { todo, onToggle } = this.props;
     return (
-      <li style={todo.completed ? { textDecoration: 'line-through' } : {}}>
+      <li
+        onClick={() => {
+          onToggle && onToggle(todo.id);
+        }}
+        style={todo.completed ? { textDecoration: 'line-through' } : {}}
+      >
         {todo.title}
       </li>
     );
@@ -26,14 +33,32 @@ class Title extends Component {
 }
 
 class App extends Component {
+  state = { todos: TODOS };
+
+  toggleTodo = todoId => {
+    const newTodos = this.state.todos.map(todo => {
+      if (todo.id === todoId) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+    this.setState({
+      todos: newTodos,
+    });
+  };
+
   render() {
     const { titleColor = 'blue' } = this.props;
+    const { todos } = this.state;
     return (
       <div className="container">
         <Title color={titleColor}>Hello world!</Title>
         <ul>
-          {TODOS.map(todo => (
-            <TodoItem todo={todo} />
+          {todos.map(todo => (
+            <TodoItem key={todo.id} todo={todo} onToggle={this.toggleTodo} />
           ))}
         </ul>
         <button>Click me!</button>
