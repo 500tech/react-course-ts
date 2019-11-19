d5d7b77bc70f74b038a0b0a85922904669a3009c
Alon Niv <oakfang@gmail.com>
2019-11-19 09:51:22 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import runAll from './es6';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-runAll();
+class App extends Component {
+  render() {
+    const { titleColor="blue" } = this.props;
+    return (
+      <div>
+        <h1 style={{ color: titleColor, backgroundColor: 'yellow' }}>
+          Hello, world!
+        </h1>
+        <p>Tagline</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App titleColor="red" />, document.querySelector('#root'));
