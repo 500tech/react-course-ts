2a18d90151c81872ba9572ec24c58b2018fd5ea1
Alon Niv <oakfang@gmail.com>
2019-11-28 10:30:43 +0200

    more links

--- src/ui/TodoItem.js
+++ src/ui/TodoItem.js
@@ -1,21 +1,25 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
 
 export class TodoItem extends PureComponent {
   render() {
     const { todo } = this.props;
 
     return (
-      <li
-        className={todo.completed ? 'completed' : null}
-        onClick={event => {
-          if (event.ctrlKey || event.metaKey) {
-            this.props.onDelete(todo.id);
-          } else {
-            this.props.onToggle(todo.id);
-          }
-        }}
-      >
-        {todo.title}
+      <li>
+        <span
+          className={todo.completed ? 'completed' : null}
+          onClick={event => {
+            if (event.ctrlKey || event.metaKey) {
+              this.props.onDelete(todo.id);
+            } else {
+              this.props.onToggle(todo.id);
+            }
+          }}
+        >
+          {todo.title}
+        </span>
+        <Link to={`/todos/${todo.id}`}>Select</Link>
       </li>
     );
   }
